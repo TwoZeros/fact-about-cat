f408b58949d125168b3cf6a40adc40cb0a465753
Kirill <pak.kiruha@yandex.ru>
2022-06-11 01:34:13 +0300

    reformat cod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -15,6 +14,7 @@ import java.util.List;
 public class Main {
     public static ObjectMapper mapper = new ObjectMapper();
     public static final String URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
     public static void main(String[] args) {
 
         CloseableHttpClient httpClient = HttpClientBuilder.create()
@@ -26,15 +26,18 @@ public class Main {
                 .build();
         HttpGet request = new HttpGet(URI);
         List<Cat> cats = new ArrayList<>();
-        try(CloseableHttpResponse response = httpClient.execute(request);){
+        try (CloseableHttpResponse response = httpClient.execute(request);) {
             cats = mapper.readValue(
                     response.getEntity().getContent(),
-                    new TypeReference<>() {});
+                    new TypeReference<>() {
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
-        cats.stream().filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+        cats.stream()
+                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
                 .sorted(Comparator.comparingLong(Cat::getUpvotes).reversed())
                 .forEach(System.out::println);
+
     }
 }
